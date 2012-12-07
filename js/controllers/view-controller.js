9dae0d13f7545bcd6a48d16da8a11d686e2dc2ed
Lev Brie <lev.brie@gmail.com>
2012-12-06 20:34:37 -0500

    Added new image functionality for thumbnails and HTML5 cover, placeholder images, special css and isotope.js for handling images, view-init.js for initializing view, and some yelp sample pages.

---
+++ js/controllers/view-controller.js
@@ -0,0 +1,32 @@
+/*
+ * This is our primary view controller.  It sets up listeners for the page, 
+ * responds to requests, and asks the models for the info it needs.
+ */
+
+var yelpObjects = []; 		// we store a list of all current YelpObject objects on the page
+							// so that any change of view that does not require a new query
+							// to the database or Yelp can be handled directly by the controller
+var currentView = "grid"; 	// we always start the program in grid view 
+var views = ['grid', 'list', 'map', 'map-list'];	// the 4 views handled by this controller 
+													// on this page (there could be others)
+
+function addListeners() {
+	var buttonToggleMap = document.getElementById('');
+	buttonToggleMap.addEventListener('click', false) {
+		
+	}
+}
+
+/*
+ * takes an arbitrary number of arguments as parameters,
+ * most of which should be Category or MightyMark Objects
+ */
+function print_args() {
+    for(var i=0; i<arguments.length; i++)
+        console.log(arguments[i])
+}
+
+var yelpObjects
+function fetchCurrentYelpObjects() {
+	
+}
--- js/views/view-init.js
+++ js/views/view-init.js
@@ -1,5 +1,4 @@
-
-/* 
+/*
  * Sets the font style to normal when "Price" is displayed on main filters button group
  * and to italic otherwise (so "expensive", for example, will be in italics)
  */
@@ -11,6 +10,7 @@ function changeTargetFont() {
 		textTarget.style.fontStyle = "italic";
 	}
 }
+
 
 $(document).ready(function() {
 
@@ -37,3 +37,94 @@ $(document).ready(function() {
 	});
 
 });
+/* from smart bootstrap */
+/* ------------------ Image Overlay ----------------- */
+
+jQuery(document).ready(function() {
+
+	$('.picture a').hover(function() {
+		$(this).find('.image-overlay-zoom, .image-overlay-link').stop().fadeTo('fast', 1);
+	}, function() {
+		$(this).find('.image-overlay-zoom, .image-overlay-link').stop().fadeTo('fast', 0);
+	});
+
+});
+
+/* -------------------- Isotope --------------------- */
+
+jQuery(document).ready(function() {
+
+	$('#portfolio-wrapper').imagesLoaded(function() {
+
+		var $container = $('#portfolio-wrapper');
+		$select = $('#filters select');
+
+		// initialize Isotope
+		$container.isotope({
+			// options...
+			resizable : false, // disable normal resizing
+			// set columnWidth to a percentage of container width
+			masonry : {
+				columnWidth : $container.width() / 12
+			}
+		});
+
+		// update columnWidth on window resize
+		$(window).smartresize(function() {
+
+			$container.isotope({
+				// update columnWidth to a percentage of container width
+				masonry : {
+					columnWidth : $container.width() / 12
+				}
+			});
+		});
+
+		$container.isotope({
+			itemSelector : '.portfolio-item'
+		});
+
+		$select.change(function() {
+
+			var filters = $(this).val();
+
+			$container.isotope({
+				filter : filters
+			});
+
+		});
+
+		var $optionSets = $('#filters .option-set'), $optionLinks = $optionSets.find('a');
+
+		$optionLinks.click(function() {
+
+			var $this = $(this);
+			// don't proceed if already selected
+			if ($this.hasClass('selected')) {
+				return false;
+			}
+			var $optionSet = $this.parents('.option-set');
+			$optionSet.find('.selected').removeClass('selected');
+			$this.addClass('selected');
+
+			// make option object dynamically, i.e. { filter: '.my-filter-class' }
+			var options = {}, key = $optionSet.attr('data-option-key'), value = $this.attr('data-option-value');
+			// parse 'false' as false boolean
+			value = value === 'false' ? false : value;
+			options[key] = value;
+			if (key === 'layoutMode' && typeof changeLayoutMode === 'function') {
+				// changes in layout modes need extra logic
+				changeLayoutMode($this, options)
+			} else {
+				// otherwise, apply new options
+				$container.isotope(options);
+			}
+
+			return false;
+
+		});
+
+	});
+
+});
+
