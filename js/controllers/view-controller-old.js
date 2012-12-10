62121b2a6e649c7580330240bb9ed892f04f8d67
Lev Brie <lev.brie@gmail.com>
2012-12-10 06:10:18 -0500

    Moved all view-related javascript from index.html into view-controller.js, moved old view-controller.js to view-controller-old.js because those functions are currently not being used although they should be eventually, cleaned up index.html file and fixed search issue for simple non-category searches, but not for ones involving found categories.

---
+++ js/controllers/view-controller-old.js
@@ -0,0 +1,73 @@
+var yelpObjects = []; 		// we store a list of all current YelpObject objects on the page
+							// so that any change of view that does not require a new query
+							// to the database or Yelp can be handled directly by the controller
+var currentView = "grid"; 	// we always start the program in grid view 
+var views = ['grid', 'list', 'map', 'map-list'];	// the 4 views handled by this controller 
+													// on this page (there could be others)
+
+function addListeners() {
+	var buttonToggleMap = document.getElementById('');
+	buttonToggleMap.addEventListener('click', false) {
+		
+	}
+}
+
+// this should be changed into a listener for when a user submits 
+// a search
+function performSearch() {
+	var searchElement = document.getElementById('chosen-select');
+	alert("Alert: " + searchElement.value);
+}
+
+
+jQuery(function($) {
+	$('#search-button').click(function() {
+		alert("WORKING");
+		$('.result-selected').each(function() {
+			alert("ALERT " + $(this).value);
+		});
+	});
+	// Runs search on enter
+	// $("#search-button").keypress(function(event) {
+		// $('.result-selected').each(function() {
+			// alert("ALERT " + $(this).value);
+		// });
+	// });
+});
+
+
+// update the view with results from query
+function updateResults(yelpResponseText, mightyResponseText) {
+	
+}
+
+// temp method to choose update yelp results
+function updateYelpResults(yelpResponseText) {
+	var resultsDiv = document.getElementById("grid-wrapper");
+	resultsDiv.innerHTML = yelpResponseText;
+	var reults = JSON.parse(yelpResponseText);
+	for(var i=0; i < results.length; i++) {
+		var result = results[i];
+		var div = document.createElement("div");
+		div.setAttribute("class", "grid-item");
+		div.innerHTML = result.name;
+		resultsDiv.appendChild(div);
+	}
+}
+/*
+ * takes an arbitrary number of arguments as parameters,
+ * most of which should be Category or MightyMark Objects
+ */
+function print_args() {
+    for(var i=0; i<arguments.length; i++)
+        console.log(arguments[i])
+}
+
+var yelpObjects
+function fetchCurrentYelpObjects() {
+	
+}
+
+function performSearch() {
+	
+}
--- js/controllers/view-controller.js
+++ js/controllers/view-controller.js
@@ -3,76 +3,166 @@
  * responds to requests, and asks the models for the info it needs.
  */
 
-var yelpObjects = []; 		// we store a list of all current YelpObject objects on the page
-							// so that any change of view that does not require a new query
-							// to the database or Yelp can be handled directly by the controller
-var currentView = "grid"; 	// we always start the program in grid view 
-var views = ['grid', 'list', 'map', 'map-list'];	// the 4 views handled by this controller 
-													// on this page (there could be others)
+			var currentView = "grid";		// set current view to the default view, which is grid
+			jQuery(function($) {			// using failsafe $ alias to ensure jQuery loading
+				// listeners for view choice buttons
+				document.getElementById("display-grid").onclick = function() {
+					if(currentView != "grid") {
+						deleteView(currentView);
+						currentView = "grid";
+						displayView("grid");
+					}					
+				};
+				document.getElementById("display-list").onclick = function() {
+					if(currentView != "list") {
+						deleteView(currentView);
+						currentView = "list";
+						displayView("list");
+					}					
+				};
+				document.getElementById("display-map").onclick = function() {
+					if(currentView != "map") {						
+						deleteView(currentView);
+						currentView = "map";
+						displayView("map");
+					}					
+				};
+			});
+			
+			function deleteView(view) {
+				switch(view) {
+					case "list": 
+						document.getElementById('list-wrapper').innerHTML = "";
+						break;
+					case "grid":
+						document.getElementById('grid-wrapper').innerHTML = "";
+						break;
+					case "map":
+						document.getElementById('map-wrapper').innerHTML = "";
+						break;
+					default: 
+						document.getElementById('map-wrapper').innerHTML = "";
+				}				
+			}
+			
+			function displayView(view) {
+				switch(view) {
+					case "list": 
+						displayList();
+						break;
+					case "grid":
+						displayGrid();
+						break;
+					case "map":
+						displayMap()
+						break;
+					default: 
+						displayGrid();
+				}				
+			}
 
-function addListeners() {
-	var buttonToggleMap = document.getElementById('');
-	buttonToggleMap.addEventListener('click', false) {
-		
-	}
-}
+			function displayGrid() {
+				var businesses = resultsData.businesses;
+				var divText = "";
 
-// this should be changed into a listener for when a user submits 
-// a search
-function performSearch() {
-	var searchElement = document.getElementById('chosen-select');
-	alert("Alert: " + searchElement.value);
-}
+				for(var i = 0; i < businesses.length; i++) {
+					var yelpObject = businesses[i];
+					divText += "<div class='span3 grid-item'><div class='picture'>";			
+					divText += "<a href='" + yelpObject.url + "' title='Title'>";
+					divText += "<img src='" + yelpObject.image_url + "' alt=''/>";
+					divText += "<div class='image-overlay-link'></div>";
+					divText += "</a>";		
+					divText += "<div class='item-description alt'>";
+					divText += "<h5><a href='project.html'>" + yelpObject.name + "</a></h5>";
+					divText += catsToHTML(yelpObject);
+					divText += reviewToHTML(yelpObject.snippet_text);
+					// divText += "<p>" + yelpObject.snippet_text + "</p>";
+					divText += "</div>";
+					divText += "<div class='post-meta'>";
+					divText += "<span><i class='mini-ico-comment'></i>  " + yelpObject.review_count + " Reviews</span><span><i class='mini-icoNN-iphone'></i> <a href='#'>  " + yelpObject.phone + "  " + " </a></span><span><i class='mini-ico-tags'></i> <a href='#'> " + "  No tags yet!!" + "</a></span>";
+					divText += "</div>";
+					divText += "</div><!-- end picture --></div><!-- end grid-item -->";	
+				}
+				// addIsotopeItems(divText);
+				// var $newItems = $(divText);
+				// $('#grid-wrapper').isotope( 'insert', $newItems );
 
+				// now wire click events for each
+				// for(i = 0; i<20; i++) {
+					// var currimg = i+1;
+					// var imgId = "image-" + currimg;
+					// alert("imgId - " + imgId);
+					// var elem = document.getElementById(imgId)
+					// alert("elem: " + elem.innerHTML);
+					// elem.onclick = function(evt) { displaySliderWith(currimg); };
+				// }
+				document.getElementById('grid-wrapper').innerHTML = divText;
+			}
+			
+			function displayList() {
+				var businesses = resultsData.businesses;
+				var divText = "";
 
-jQuery(function($) {
-	$('#search-button').click(function() {
-		alert("WORKING");
-		$('.result-selected').each(function() {
-			alert("ALERT " + $(this).value);
-		});
-	});
-	// Runs search on enter
-	// $("#search-button").keypress(function(event) {
-		// $('.result-selected').each(function() {
-			// alert("ALERT " + $(this).value);
-		// });
-	// });
-});
+				for(var i = 0; i < businesses.length; i++) {
+					var yelpObject = businesses[i];
+					divText += "<div class='list-item row'>";			
+					divText += "<div class='span2'><img src='" + yelpObject.image_url + "' alt=''/></div>";
+					divText += "<div class='image-overlay-link'></div>";	
+					divText += "<div class='span8'><h5>";
+					divText += "<a href='" + yelpObject.url + "' title='" + yelpObject.name + "'>";
+					divText += yelpObject.name + "</a></h5>";
+					divText += catsToHTML(yelpObject);
+					divText += reviewToHTML(yelpObject.snippet_text);
+					divText += "<span><i class='mini-ico-comment'></i>  " + yelpObject.review_count + " Reviews</span><span><i class='mini-icoNN-iphone'></i> <a href='#'>  " + yelpObject.phone + "  " + " </a></span><span><i class='mini-ico-tags'></i> <a href='#'> " + "  No tags yet!!" + "</a></span>";
+					divText += "</div>";
+					divText += "<div class='span1 pull-right'><a class='btn collapsed' href='#'>More <span class='caret'></span></a></div>";
+					divText += "</div><!-- end list-item -->";	
+				}
+				document.getElementById('list-wrapper').innerHTML = divText;
+			}
+			
+			function catsToHTML(obj) {
+				if(obj.categories) {
+					var cats = obj.categories;
+					var text = "<h6>Categories: ";
+					for(var j = 0; j < cats.length; cats++) {
+						text += cats[j][0] + " ";
+					}
+					text += "</h6>";		
+					return text;
+				}
+			}
+			
+			// converts the review snippet to html to hold review in a nice content bubble
+			function reviewToHTML(snippet) {
+				var text = "<div class='review-container'>";
+				text += "<div class='review'><p>";
+				text += snippet;
+				text += "</p></div>";
+				text += "<div class='review-bg'></div>";
+				text += "<div class='review-author'>Anonymous Yelp Reviewer</div>";
+				text += "</div>";
+				return text;
+			}
+			
+			function createBookmarkPopover(yelpid) {
+				var text = "<a class='btn' data-content='<form class='form-inline'>";
+				text += "'<input type='text' class='popover-input' placeholder='Start a new MightyList...'>";
+				text += "<!-- <button type='submit' class='btn'>Add</button> -->";
+				text += "</form>";
+				text += createPopoverList(yelpid);
+				text += "rel='popover' href='#' data-original-title='Add a bookmark:'";
+				text += " data-html='true'>Click to toggle popover</a>";
+				return text;
+			}
+			
+			function createPopoverList(yelpid) {
+				var text = "";
+				for(var i=0; i < bookmarkList.length; i++) {
+					text += "<a class='popover-item' href='levbrie.com' data-list-name='";
+					text += bookmarkList.id + "' data-yelpid='" + yelpid + "' title=''><span>";
+					text += bookmarkList.listName + "</span></a>";
+				}
+				return text;
+			}
 
-
-// update the view with results from query
-function updateResults(yelpResponseText, mightyResponseText) {
-	
-}
-
-// temp method to choose update yelp results
-function updateYelpResults(yelpResponseText) {
-	var resultsDiv = document.getElementById("grid-wrapper");
-	resultsDiv.innerHTML = yelpResponseText;
-	var reults = JSON.parse(yelpResponseText);
-	for(var i=0; i < results.length; i++) {
-		var result = results[i];
-		var div = document.createElement("div");
-		div.setAttribute("class", "grid-item");
-		div.innerHTML = result.name;
-		resultsDiv.appendChild(div);
-	}
-}
-/*
- * takes an arbitrary number of arguments as parameters,
- * most of which should be Category or MightyMark Objects
- */
-function print_args() {
-    for(var i=0; i<arguments.length; i++)
-        console.log(arguments[i])
-}
-
-var yelpObjects
-function fetchCurrentYelpObjects() {
-	
-}
-
-function performSearch() {
-	
-}
